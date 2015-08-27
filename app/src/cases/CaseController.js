2dc8d49c97ca4d574180d3637018e2ca8cb1a7ec
frank <frank.thomsen@gmail.com>
2015-08-27 13:55:32 +0200

    examples of data binding in cases view

--- app/src/cases/CaseController.js
+++ app/src/cases/CaseController.js
@@ -47,8 +47,72 @@
         journalKey: 'KGHEISHG',
         isUpdated: false,
         documents: ['document1.xls', 'document2.pdf', 'document3.xls']
+      },
+      {
+        caseId: 275,
+        title: 'It is a case with case stuff in it',
+        desc: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.',
+        type: 'Casetype',
+        createdDate: 'May 2 2015',
+        startDate: 'May 2 2015',
+        modifiedDate: 'May 12 2015',
+        endDate: 'June 21 2015',
+        status: 'In progress',
+        owner: 'Someone Someonesson',
+        creator: 'Svend Tveskæg',
+        assignee: '',
+        journalKey: 'KGHEISHG',
+        isUpdated: false,
+        documents: ['document1.xls', 'document2.pdf', 'document3.xls']
+      },
+      {
+        caseId: 123275,
+        title: 'Yet another case',
+        desc: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.',
+        type: 'Casetype',
+        createdDate: 'May 2 2015',
+        startDate: 'May 2 2015',
+        modifiedDate: 'May 12 2015',
+        endDate: 'June 21 2015',
+        status: 'In progress',
+        owner: 'Someone Someonesson',
+        creator: 'Svend Tveskæg',
+        assignee: '',
+        journalKey: 'KGHEISHG',
+        isUpdated: false,
+        documents: ['document1.xls', 'document2.pdf', 'document3.xls']
       }
     ];
+    
+    $scope.myUpdatedCases = countMyUpdatedCases();
+    
+    function countMyUpdatedCases() {
+      var i = 0;
+      for (var c in $scope.cases) {
+        if ($scope.cases[c].isUpdated) {
+          i++;
+        };
+      }
+      return i;
+    };
+    
+    $scope.unassignedCases = countUnassignedCases();
+    
+    function countUnassignedCases() {
+      var i = 0;
+      for (var c in $scope.cases) {
+        if ($scope.cases[c].assignee === '') {
+          i++;
+        };
+      }
+      return i;
+    };
+    
+    $scope.casesNeedAction = countCasesNeedAction();
+    
+    function countCasesNeedAction() {
+      return countUnassignedCases() + countMyUpdatedCases();
+    };
     
     $scope.createCase = function(ev) {
       $mdDialog.show({
