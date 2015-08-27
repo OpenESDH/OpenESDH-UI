e6e645e86d13e8d309cc95473c382a964a728587
frank <frank.thomsen@gmail.com>
2015-08-27 15:52:43 +0200

    added documents list

---
+++ app/src/documents/DocumentController.js
@@ -0,0 +1,47 @@
+(function(){
+
+  angular
+       .module('documents')
+       .controller('DocumentController', ['$scope', DocumentController]);
+
+  function DocumentController($scope) {
+  
+    $scope.docs = [
+      {
+        title: 'Document title',
+        docNo: 4686297957,
+        type: 'Invoice',
+        category: 'Accounting',
+        state: 'Draft',
+        owner: 'Someone Someonesson',
+        createdDate: 'June 12, 2015',
+        modifiedDate: 'June 23, 2015',
+        isUpdated: true
+      },
+      {
+        title: 'Another document title',
+        docNo: 154,
+        type: 'Invoice',
+        category: 'Accounting',
+        state: 'Draft',
+        owner: 'Someone Someonesson',
+        createdDate: 'June 12, 2015',
+        modifiedDate: 'June 23, 2015',
+        isUpdated: true
+      },
+      {
+        title: 'Title title title',
+        docNo: 286684,
+        type: 'Invoice',
+        category: 'Accounting',
+        state: 'Draft',
+        owner: 'Someone Someonesson',
+        createdDate: 'June 12, 2015',
+        modifiedDate: 'June 23, 2015',
+        isUpdated: false
+      }
+    ];
+  
+  }
+
+})();
