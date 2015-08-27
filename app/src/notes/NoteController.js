b93d5291eb1378ee9d242f97e364243c90fc0dd7
frank <frank.thomsen@gmail.com>
2015-08-27 16:27:37 +0200

    added notes controller

---
+++ app/src/notes/NoteController.js
@@ -0,0 +1,36 @@
+(function(){
+
+  angular
+       .module('notes')
+       .controller('NoteController', ['$scope', NoteController]);
+
+  function NoteController($scope) {
+  
+    $scope.notes = [
+      {
+        title: 'Note title',
+        desc: 'This is a note. eglsiehf lsæeifhj slefij slæefihs elfijs eflihs eflsihef lsjef lsief lsefj sliefh slefj slefh sleifj lseifh sleifh slefihs lefihs elf',
+        createdDate: 'June 12, 2015',
+        creator: 'Someone Someonesson',
+        relatedTo: [
+          'William Shakespeare',
+          'Mogens Glistrup',
+          'Ole Lund Kirkeby'
+        ]
+      },
+      {
+        title: 'Another note',
+        desc: 'This is a note. eglsiehf lsæeifhj slefij slæefihs elfijs eflihs eflsihef lsjef lsief lsefj sliefh slefj slefh sleifj lseifh sleifh slefihs lefihs elf',
+        createdDate: 'June 12, 2015',
+        creator: 'Someone Someonesson',
+        relatedTo: [
+          'William Shakespeare',
+          'Mogens Glistrup',
+          'Ole Lund Kirkeby'
+        ]
+      }
+    ];
+  
+  }
+
+})();
