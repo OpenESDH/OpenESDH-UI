a9ae8d83e364aedd0eb500a4a858247b00941a5c
frank <frank.thomsen@gmail.com>
2015-08-31 17:08:08 +0200

    last commit before moving to git flow. added templates for managing organisations (/organisations and /organisations/organisation)

--- app/src/app.module.js
+++ app/src/app.module.js
@@ -1,7 +1,7 @@
 (function() {
 
   angular
-    .module('openeApp', ['ngMaterial', 'ngRoute', 'ngResource', 'openeApp.cases', 'openeApp.dashboard', 'openeApp.files', 'openeApp.tasks', 'openeApp.documents', 'openeApp.notes'])
+    .module('openeApp', ['ngMaterial', 'ngRoute', 'ngResource', 'openeApp.cases', 'openeApp.dashboard', 'openeApp.files', 'openeApp.tasks', 'openeApp.documents', 'openeApp.notes', 'openeApp.organisations'])
     .config(config);
 
   config.$inject = ['$mdThemingProvider', '$routeProvider'];
@@ -39,6 +39,14 @@
         .when('/tasks/', {
           controller: 'TaskController',
           templateUrl: 'app/src/tasks/view/tasks.html'
+        })
+        .when('/organisations/', {
+          controller: 'OrganisationController',
+          templateUrl: 'app/src/organisations/view/organisations.html'
+        })
+        .when('/organisations/organisation', {
+          controller: 'OrganisationController',
+          templateUrl: 'app/src/organisations/view/organisation.html'
         })
         .otherwise({
           redirectTo: '/'
