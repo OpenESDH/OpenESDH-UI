e6a8275a33584e2a36fcc66ce743369fb2bc9293
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2015-08-31 16:13:04 +0300

    OPENE-237 Added new case note creation dialog.

--- app/src/notes/NoteController.js
+++ app/src/notes/NoteController.js
@@ -4,14 +4,14 @@
        .module('openeApp.notes')
        .controller('NoteController', NoteController);
 
-    NoteController.$inject = ['$scope', '$routeParams', 'caseNotesService'];
+    NoteController.$inject = ['$scope', '$routeParams', '$mdDialog', 'caseNotesService'];
 
-    function NoteController($scope, $routeParams, caseNotesService) {
+    function NoteController($scope, $routeParams, $mdDialog, caseNotesService) {
 
         var caseId = $routeParams.caseId;
         
         var vm = this;
-        vm.addNote = addNote;
+        vm.newNote = newNote;
         vm.loadNotes = loadNotes;
         vm.pageSize = 2;
         
@@ -25,7 +25,6 @@
             var res = caseNotesService.getCaseNotes(caseId, page, vm.pageSize);
             res.then(function(response) {     
                 vm.notes = response.notes;
-                console.log("notes", response);
                 vm.contentRange = response.contentRange;
                 var pages = [];
                 var pagesCount = Math.ceil(response.contentRange.totalItems / vm.pageSize); 
@@ -36,25 +35,45 @@
             });
         }
         
-        function addNote(){
-            var partiesNodeRefs = [];
-            var newNote = vm.newNote;
-            for(var i in newNote.concernedParties){
-                var party = newNote.concernedParties[i];
-                partiesNodeRefs.push(party.nodeRef);
+        function newNote(ev){
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'app/src/notes/noteCrudDialog.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose:true
+              })
+              .then(function(note) {
+                  addNote(note);
+              }, function() {
+                //on cancel dialog
+              });
+        }
+        
+        function addNote(note){
+            
+            if(!note || (!note.headline && !note.content)){
+                return;
             }
-            var note = {
-                    parent: newNote.parent,
-                    headline: newNote.headline,
-                    content: newNote.content,
-                    concernedParties: partiesNodeRefs
-            };
+            
+            var partiesNodeRefs = [];
+            note.concernedParties = partiesNodeRefs;
             caseNotesService.addNewNote(caseId, note).then(function(response){
-               newNote.headline = "";
-               newNote.content = "";
-               newNote.concernedParties = [];
+                loadNotes(1);
             });
         }
+        
+        function DialogController($scope, $mdDialog) {
+            $scope.hide = function() {
+              $mdDialog.hide();
+            };
+            $scope.cancel = function() {
+              $mdDialog.cancel();
+            };
+            $scope.addNote = function(){                
+                $mdDialog.hide({headline: $scope.headline, content: $scope.content});
+            };
+        };
     }
 
 })();
