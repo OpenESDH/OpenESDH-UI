e456f6c80be3fb766c1ab9237d3ffcb19bc5dc7a
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2015-09-01 10:41:27 +0300

    OPENE-245 Angular service to upload files into Alfresco.

--- app/src/documents/DocumentController.js
+++ app/src/documents/DocumentController.js
@@ -1,21 +1,27 @@
 (function(){
     
-    angular.module('openeApp.documents').controller('DocumentController',
-            DocumentController);
+    angular
+        .module('openeApp.documents')
+        .controller('DocumentController', DocumentController);
     
-    DocumentController.$inject = [ '$scope', '$routeParams', 'caseDocumentsService' ];
+    DocumentController.$inject = [ '$scope', '$routeParams', '$mdDialog', 'caseDocumentsService' ];
     
-    function DocumentController($scope, $routeParams, caseDocumentsService) {
+    function DocumentController($scope, $routeParams, $mdDialog, caseDocumentsService) {
     
         var caseId = $routeParams.caseId;
+        var caseDocsFolderNodeRef = '';
         var vm = this;
         vm.pageSize = 2;
         
         vm.loadDocuments = loadDocuments;
+        vm.uploadDocument = uploadDocument; 
         
         activate();
         
         function activate(){
+            caseDocumentsService.getDocumentsFolderNodeRef(caseId).then(function(res){
+                caseDocsFolderNodeRef = res.caseDocsFolderNodeRef;
+            });
             loadDocuments(1);
         }
         
@@ -31,6 +37,37 @@
                 }
                 vm.pages = pages;
             });
+        }
+        
+        function uploadDocument(ev){
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'app/src/documents/documentUploadDialog.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose:true
+            })
+            .then(function(fileToUpload) {
+                if(!fileToUpload){
+                    return;
+                }
+                caseDocumentsService.uploadCaseDocument(fileToUpload, caseDocsFolderNodeRef).then(function(result){
+                    loadDocuments(1);
+                });
+            }, function() {
+                //on cancel dialog
+            });
+        }
+        
+        function DialogController($scope, $mdDialog) {
+            
+            $scope.cancel = function() {
+              $mdDialog.cancel();
+            };
+            
+            $scope.upload = function(){
+                $mdDialog.hide($scope.fileToUpload);
+            };
         }
     }
 
---
+++ app/src/shared/services/alfrescoUploadService.js
@@ -0,0 +1,54 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('openeApp')
+        .factory('alfrescoUploadService', AlfrescoUploadService);
+
+    AlfrescoUploadService.$inject = ['$http'];
+
+    function AlfrescoUploadService($http) {
+        
+        var service = {
+            uploadFile: uploadFile
+        };
+        return service;
+        
+        function uploadFile(file, destination, extras){
+            
+            var formData = new FormData();
+            formData.append("filedata", file);
+            formData.append("filename", file.name);
+            formData.append("destination", destination ? destination : null);
+
+            if(!extras || !extras.majorVersion){
+                formData.append("majorVersion", "false");    
+            }
+            
+            /**
+             * optional fields which may be passed via extras:
+             * 
+             *      siteId
+             *      containerId
+             *      uploaddirectory
+             *      username
+             *      overwrite
+             *      thumbnails
+             */
+            
+            if(extras){
+                angular.forEach(extras, function (value, key) {
+                    formData.append(key, value);
+                });
+            }
+            
+            return $http.post("/alfresco/service/api/upload", formData,  {
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined}
+            }).then(function(response){
+                return response;
+            });
+        }
+        
+    }
+})();
