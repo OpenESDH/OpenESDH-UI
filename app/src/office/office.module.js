dd5a3bf05b4fd1988224f53ddbd012436389a763
Torben N. Rasmussen <Torben.Rasmussen@visma.com>
2015-09-03 15:19:50 +0200

    struggling with config

--- app/src/office/office.module.js
+++ app/src/office/office.module.js
@@ -1,11 +1,8 @@
 (function(){
     'use strict';
 
-    console.log('asdf');
     angular.module('openeApp.office', [
         'ngMaterial',
         'ui.router'
-    ]).config(function() {
-              console.log('documents');
-          });
+    ]);
 })();
