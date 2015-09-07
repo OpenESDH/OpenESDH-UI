42d8ed6425ed39fae952e57fed3394948ddda9e2
Seth Yastrov <seth@magenta.dk>
2015-09-07 11:22:27 +0200

    Changes to create case dialog: - Set first field in dialog to automatically focus when dialog is shown. - Make case start date field optional. = Remove case status field.

---
+++ app/src/shared/directives/focusMeDirective.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('openeApp')
+        .directive('focusMe', FocusMeDirective);
+
+    FocusMeDirective.$inject = ['$timeout'];
+
+    function FocusMeDirective($timeout) {
+        return {
+            scope: { trigger: '@focusMe' },
+            link: function(scope, element) {
+                scope.$watch('trigger', function(value) {
+                    if(value === "true") {
+                        $timeout(function() {
+                            element[0].focus();
+                        });
+                    }
+                });
+            }
+        };
+    };
+})();
