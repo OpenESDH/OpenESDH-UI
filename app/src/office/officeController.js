fe47b3fc2f2103a3eed58d0d59b473484d0486a8
frank <frank.thomsen@gmail.com>
2015-09-10 13:52:01 +0200

    add autocomplete to outlook

--- app/src/office/officeController.js
+++ app/src/office/officeController.js
@@ -7,16 +7,16 @@
 
     var email;
 
-    OfficeController.$inject = ['$window', 'officeService', 'caseService'];
+    OfficeController.$inject = ['$window', 'officeService', 'caseService', '$q'];
 
-    function OfficeController($window, officeService, caseService) {
+    function OfficeController($window, officeService, caseService, $q) {
         var vm = this;
-
         vm.email = email;
         vm.from = email.From.EMail;
         vm.subject = email.Subject;
         vm.caseId;
         vm.attachments = email.Attachments;
+        
         vm.save = save;
         vm.cancel = cancel;
 
@@ -41,6 +41,24 @@
         function cancel() {
             $window.external.CancelOpenEsdh();
         }
+        
+        
+        /*
+         * Autocomplete input
+         */
+        
+        vm.querySearch = querySearch;
+        function querySearch (query) {
+          var allTheCases =  [
+            { title: 'Case 1', id: '2342' },
+            { title: 'Case 2', id: '52' },
+            { title: 'Case 3', id: '74' },
+            { title: 'Case 4', id: '97663' },
+            { title: 'Case 5', id: '35751' }
+          ];
+          return allTheCases;
+        }
+        
     }
 
     // This function is called from the Office Add-In
