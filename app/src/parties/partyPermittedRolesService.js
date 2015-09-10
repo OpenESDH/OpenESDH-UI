d01f50b7315ffcd1ac443dfea147769d2d9dd899
Arnas Petraitis <arnas.petraitis@visma.com>
2015-09-10 17:40:18 +0300

    case parties angular service and view

---
+++ app/src/parties/partyPermittedRolesService.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+
+    angular
+            .module('openeApp.parties')
+            .factory('partyPermittedRolesService', PartyPermittedRolesService);
+
+    PartyPermittedRolesService.$inject = ['$http', '$q'];
+
+    function PartyPermittedRolesService($http, $q) {
+        var service = {
+            getRoles: getRoles
+        };
+        return service;
+
+        function getRoles() {
+            return $http.get("/alfresco/service/api/openesdh/case/party/permittedRoles")
+                    .then(successOrReject);
+        }
+
+        function successOrReject(response) {
+            if (response.status && response.status !== 200) {
+                return $q.reject(response);
+            }
+            return response.data;
+        }
+    }
+})();
