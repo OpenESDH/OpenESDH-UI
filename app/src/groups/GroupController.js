1115bb975b743aa75d3f3edaae3b388dd0d2e61d
frank <frank.thomsen@gmail.com>
2015-09-18 11:51:39 +0200

    function definitions added to GroupController

--- app/src/groups/GroupController.js
+++ app/src/groups/GroupController.js
@@ -13,7 +13,25 @@
      * @constructor
      */
     function GroupController($scope, $mdDialog) {
-        var vm = this;
+        
+        // Return an array of group objects
+        function listGroups() {};
+        
+        // !! Probably something that must be done with routes. !!
+        // Display the group view and load it with one group object
+        function showGroup( group_shortName ) {};
+        
+        // Return an array of user and group objects belonging to this group
+        function listMembers( group_shortName ) {};
+        
+        // Add a user or subgroup to group
+        function addMember( group_shortName ) {};
+        
+        // Remove member from group
+        function removeMember( group_shortName, member_shortName ) {};
+        
+        // Create a new group, then go to this group's view
+        function createGroup( shortName, displayName ) {};
         
     };
   
