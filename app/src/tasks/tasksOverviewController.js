71e5377d1a4a12235cacf2fc45e0ceea5dc91c5d
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2015-09-29 13:36:43 +0300

    OPENE-289 Delete button for tasks.

--- app/src/tasks/tasksOverviewController.js
+++ app/src/tasks/tasksOverviewController.js
@@ -4,16 +4,33 @@
     angular.module('openeApp.tasks')
         .controller('tasksOverviewController', TasksOverviewController);
     
-    TasksOverviewController.$inject = [ '$scope', 'taskService' ];
+    TasksOverviewController.$inject = [ '$scope', 'taskService', '$mdDialog', '$translate', 'workflowService'];
     
-    function TasksOverviewController($scope, taskService) {
+    function TasksOverviewController($scope, taskService, $mdDialog, $translate, workflowService) {
         var vm = this;
+        vm.deleteTask = deleteTask;
         
         init();
         
         function init(){
             taskService.getCurrentUserWorkflowTasks().then(function(result){
                 vm.tasks = result;
+            });
+        }
+        
+        function deleteTask(task){
+            console.log(task);
+            var confirm = $mdDialog.confirm()
+                .title($translate.instant('COMMON.CONFIRM'))
+                .content($translate.instant('WORKFLOW.ARE_YOU_SURE_YOU_WANT_TO_DELETE_THE_TASK_AND_WORKFLOW', {task_description: task.properties.bpm_description}))
+                .ariaLabel('')
+                .targetEvent(null)
+                .ok($translate.instant('COMMON.YES'))
+                .cancel($translate.instant('COMMON.CANCEL'));
+            $mdDialog.show(confirm).then(function() {
+                workflowService.deleteWorkflow(task.workflowInstance.id).then(function(result){
+                    init();
+                });
             });
         }
         
