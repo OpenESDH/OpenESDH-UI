f950b7650114af820bd9c06188f9a991da40c4d9
Lanre <el_gigantes@hotmail.co.uk>
2015-10-29 16:01:50 +0100

    downgrading selenium jar for test

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -9,7 +9,7 @@ exports.config = {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:8080',
+  baseUrl: 'http://localhost:8000',
 
   seleniumAddress:'http://127.0.0.1:4444/wd/hub',
 
