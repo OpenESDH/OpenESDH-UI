5f4a550c11eefc7cbafd28c43c15826e4bb8e61e
Lanre <el_gigantes@hotmail.co.uk>
2015-12-10 11:01:24 +0100

    The ability to store case type(s) metadata with the document template

--- app/src/officeTemplates/OfficeTemplateController.js
+++ app/src/officeTemplates/OfficeTemplateController.js
@@ -1,120 +1,132 @@
+angular
+    .module('openeApp.officeTemplates')
+    .controller('OfficeTemplateController', OfficeTemplateController);
 
-    angular
-        .module('openeApp.officeTemplates')
-        .controller('OfficeTemplateController', OfficeTemplateController);
+/**
+ * Main Controller for the Cases module
+ * @param $scope
+ * @param cases
+ * @constructor
+ */
+function OfficeTemplateController($scope, $mdDialog, $translate, officeTemplateService, caseService, FileSaver, Blob) {
+    var vm = this;
 
-    /**
-     * Main Controller for the Cases module
-     * @param $scope
-     * @param cases
-     * @constructor
-     */
-    function OfficeTemplateController($scope, $mdDialog, $translate, officeTemplateService, FileSaver, Blob) {
-        var vm = this;
+    vm.getTemplates = getTemplates;
+    vm.deleteTemplate = deleteTemplate;
+    vm.getTemplate = getTemplate;
+    vm.getThumbnail = getThumbnail;
+    vm.fillTemplate = fillTemplate;
+    vm.getFileExtension = getFileExtension;
+    vm.uploadNewTemplate = uploadTemplate;
 
-        vm.getTemplates = getTemplates;
-        vm.deleteTemplate = deleteTemplate;
-        vm.getTemplate = getTemplate;
-        vm.getThumbnail = getThumbnail;
-        vm.fillTemplate = fillTemplate;
-        vm.getFileExtension = getFileExtension;
-        vm.uploadNewTemplate = uploadTemplate;
+    activate();
 
-        activate();
+    function activate() {
+        getTemplates();
+    }
 
-        function activate() {
-            getTemplates();
-        }
+    function getTemplates() {
+        return officeTemplateService.getTemplates().then(function (templates) {
+            console.log("Templates: ", templates)
+            vm.templates = templates;
+        });
+    }
 
-        function getTemplates() {
-            return officeTemplateService.getTemplates().then(function(templates) {
-                console.log("Templates: ", templates)
-                vm.templates = templates;
+    function getThumbnail(nodeRef) {
+        return officeTemplateService.getCardViewThumbnail(nodeRef);
+    }
+
+    function getFileExtension(filename) {
+        var parts = filename.split('.');
+        return parts[parts.length - 1];
+    }
+
+    function deleteTemplate(ev, template) {
+        var confirm = $mdDialog.confirm()
+            .title($translate.instant('COMMON.CONFIRM'))
+            .textContent($translate.instant('DOCUMENT.TEMPLATE.ARE_YOU_SURE_YOU_WANT_TO_DELETE_TEMPLATE', {title: template.title}))
+            .targetEvent(ev)
+            .ok($translate.instant('COMMON.YES'))
+            .cancel($translate.instant('COMMON.CANCEL'));
+
+        $mdDialog.show(confirm).then(function () {
+            officeTemplateService.deleteTemplate(template.nodeRef).then(function (response) {
+                return getTemplates();
             });
-        }
+        });
+        ev.stopPropagation();
+    }
 
-        function getThumbnail(nodeRef) {
-            return officeTemplateService.getCardViewThumbnail(nodeRef);
-        }
+    function getTemplate(nodeRef) {
+        return officeTemplateService.getTemplate(nodeRef).then(function (template) {
+            return template;
+        });
+    }
 
-        function getFileExtension(filename) {
-            var parts = filename.split('.');
-            return parts[parts.length - 1];
-        }
+    function fillTemplate(template, fieldData) {
+        officeTemplateService.fillTemplate(template.nodeRef, fieldData).then(function (blob) {
+            FileSaver.saveAs(
+                blob,
+                template.name.split('.').slice(0, -1).join(".") + ".pdf"
+            );
+        });
+    }
 
-        function deleteTemplate(ev, template) {
-            var confirm = $mdDialog.confirm()
-                    .title($translate.instant('COMMON.CONFIRM'))
-                    .textContent($translate.instant('DOCUMENT.TEMPLATE.ARE_YOU_SURE_YOU_WANT_TO_DELETE_TEMPLATE', {title: template.title}))
-                    .targetEvent(ev)
-                    .ok($translate.instant('COMMON.YES'))
-                    .cancel($translate.instant('COMMON.CANCEL'));
-            
-            $mdDialog.show(confirm).then(function() {
-                officeTemplateService.deleteTemplate(template.nodeRef).then(function(response) {
-                    return getTemplates();
-                });
+    function uploadTemplate() {
+        showDialog(DocumentTemplateDialogController).then(function (response) {
+            console.log("==> Response from dialog:", response);
+            officeTemplateService.uploadTemplate(response).then(function (response) {
+                console.log("==> Response from dialog Service:", response);
+                return getTemplates();
             });
-            ev.stopPropagation();
-        }
+        });
+    }
 
-        function getTemplate(nodeRef) {
-            return officeTemplateService.getTemplate(nodeRef).then(function(template) {
-                return template;
-            });
-        }
-
-        function fillTemplate(template, fieldData) {
-            officeTemplateService.fillTemplate(template.nodeRef, fieldData).then(function (blob) {
-                FileSaver.saveAs(
-                    blob,
-                    template.name.split('.').slice(0, -1).join(".") + ".pdf"
-                );
-            });
-        }
-
-        function uploadTemplate(){
-            showDialog(NewCaseDocumentDialogController).then(function (response) {
-                console.log("==> Response from dialog:", response);
-                officeTemplateService.uploadTemplate(response).then(function(response) {
-                    console.log("==> Response from dialog Service:", response);
-                    return getTemplates();
-                });
-            });
-        }
-
-        function showDialog(controller, locals){
-            if(!locals){
-                locals = {
-                    newTemplateVersion: false
-                };
-            }
-            return $mdDialog.show({
-                controller: NewCaseDocumentDialogController,
-                templateUrl: 'app/src/officeTemplates/view/uploadDialog.html',
-                parent: angular.element(document.body),
-                targetEvent: null,
-                clickOutsideToClose: true,
-                locals: locals
-            });
-        }
-
-        function NewCaseDocumentDialogController($scope, $mdDialog) {
-
-            $scope.templateProperties = {
-                majorVersion: "false"
-            };
-
-            $scope.cancel = function() {
-                $mdDialog.cancel();
-            };
-
-            $scope.upload = function(){
-                var response = {
-                    fileToUpload: $scope.fileToUpload,
-                    templateProperties: $scope.templateProperties
-                };
-                $mdDialog.hide(response);
+    function showDialog(controller, locals) {
+        if (!locals) {
+            locals = {
+                newTemplateVersion: false
             };
         }
-  }
+        return $mdDialog.show({
+            controller: DocumentTemplateDialogController,
+            controllerAs: "dtdc",
+            templateUrl: 'app/src/officeTemplates/view/uploadDialog.html',
+            parent: angular.element(document.body),
+            targetEvent: null,
+            clickOutsideToClose: true,
+            locals: locals
+        });
+    }
+
+    function DocumentTemplateDialogController($scope, $mdDialog) {
+        var tdc = this;
+
+        tdc.selectedTypes=null;
+        tdc.registeredTypes=null;
+        tdc.registeredCaseTypes = getRegisteredCaseTypes();
+
+        $scope.templateProperties = {
+            majorVersion: "false"
+        };
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        };
+        $scope.upload = function () {
+            $scope.templateProperties.caseTypes = tdc.selectedTypes
+            var response = {
+                fileToUpload: $scope.fileToUpload,
+                templateProperties: $scope.templateProperties
+            };
+            debugger;
+            $mdDialog.hide(response);
+        };
+
+        function getRegisteredCaseTypes() {
+            return caseService.getRegisteredCaseTypes().then(function(response) {
+                tdc.registeredTypes = response;
+                return response;
+            });
+        }
+    }
+}
