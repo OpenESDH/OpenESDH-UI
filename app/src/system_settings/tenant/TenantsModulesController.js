22ef886c3305c571897445ee9c1322f841b6343c
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2016-01-26 10:07:59 +0200

    OPENE-428 New tenant creation dialog.

--- app/src/system_settings/tenant/TenantsModulesController.js
+++ app/src/system_settings/tenant/TenantsModulesController.js
@@ -2,11 +2,12 @@
         .module('openeApp.systemsettings')
         .controller('TenantsModulesController', TenantsModulesController);
     
-    function TenantsModulesController(tenantsService, $mdDialog, $translate){
+    function TenantsModulesController(tenantsService, $mdDialog, $translate, notificationUtilsService){
         var vm = this;
         vm.deleteModules = deleteModules;
-        vm.editTenant = editTenant;
-        vm.newTenant = newTenant;
+        vm.editTenantModules = editTenantModules;
+        vm.newTenantModules = newTenantModules;
+        vm.createTenant = createTenant;
         
         loadList();
         
@@ -36,17 +37,53 @@
             });
         }
         
-        function editTenant(ev, tenant, modules){
+        function editTenantModules(ev, tenant, modules){
             showDialog(ev, tenant, modules);    
         }
         
-        function newTenant(ev){
+        function newTenantModules(ev){
             tenantsService.getAllTenants().then(function(tenants){
                 vm.tenants = tenants.filter(function(tenant){
                     return vm.tenantsModules[tenant] == undefined;
                 });
                 showDialog(ev);
             });
+        }
+        
+        function createTenant(ev){
+            $mdDialog.show({
+                controller: TenantDialogController,
+                controllerAs: 'tc',
+                templateUrl: 'app/src/system_settings/tenant/view/tenantCrudDialog.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
+            });
+        }
+        
+        function TenantDialogController($scope, $mdDialog){
+            var tc = this;
+            tc.cancel = cancel;
+            tc.save = save;
+            tc.tenant = {};
+            
+            function cancel(){
+                $mdDialog.cancel();
+            }
+            
+            function save(form){
+                if (!form.$valid) {
+                    return;
+                }
+                $mdDialog.hide();
+                tenantsService.createTenant(tc.tenant).then(function(response){
+                    notificationUtilsService.notify($translate.instant('ADMIN.SYS_SETTINGS.TENANTS_MODULES.TENANT_CREATED', tc.tenant));
+                }, saveError);
+            }
+            
+            function saveError(response) {
+                console.log(response);
+            }
         }
         
         function showDialog(ev, tenant, modules){
