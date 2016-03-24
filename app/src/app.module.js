7a8fcfb7e143ad10e4bfaf2e5e41b6ae350841df
Arnas Petraitis <arnas.petraitis@visma.com>
2016-03-24 16:55:19 +0200

    OPENE-534 using states for contacts in main menu. fixed navigation to edit organization and back

--- app/src/app.module.js
+++ app/src/app.module.js
@@ -254,15 +254,51 @@
         }).state('contacts', {
             parent: 'site',
             url: '/contacts',
+            data: {
+                authorizedRoles: [USER_ROLES.user],
+                selectedTab: 0
+            },
             views: {
                 'content@': {
                     templateUrl: 'app/src/contacts/view/contacts.html',
-                    controller: 'AdminController',
+                    controller: 'ContactsController',
                     controllerAs: 'vm'
+                }
+            }
+        }).state('contacts.persons', {
+            url: '/persons',
+            data: {
+                authorizedRoles: [USER_ROLES.user],
+                selectedTab: 0,
+                hideHeader: true
+            },
+            views: {
+                'persons': {
+                    templateUrl: 'app/src/contacts/view/persons.html'
+                }
+            }
+        }).state('contacts.organizations', {
+            url: '/organizations',
+            data: {
+                authorizedRoles: [USER_ROLES.user],
+                selectedTab: 1,
+                hideHeader: true
+            },
+            views: {
+                'organizations': {
+                    templateUrl: 'app/src/contacts/view/organizations.html'
+                }
+            }
+        }).state('contacts.organization', {
+            url: '/organization/:storeProtocol/:storeIdentifier/:uuid',
+            views: {
+                'organizations': {
+                    templateUrl: 'app/src/contacts/view/organization.html'
                 }
             },
             data: {
-                authorizedRoles: [USER_ROLES.user]
+                authorizedRoles: [USER_ROLES.user],
+                selectedTab: 1
             }
         }).state('tasks', {
             parent: 'site',
