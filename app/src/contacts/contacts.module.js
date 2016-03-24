1f800fdf2b0c42d26880dac869d0ff38873dee83
Arnas Petraitis <arnas.petraitis@visma.com>
2016-03-24 17:22:43 +0200

    moving constants to openeApp.init module; moving contacts states to openeApp.contacts module;

--- app/src/contacts/contacts.module.js
+++ app/src/contacts/contacts.module.js
@@ -1,9 +1,95 @@
 angular.module('openeApp.contacts', ['ngMaterial'])
-	.config(config);
+        .config(config);
 
-	function config(dashboardServiceProvider, modulesMenuServiceProvider) {
-        modulesMenuServiceProvider.addItem({
-            templateUrl: 'app/src/contacts/view/menuItem.html',
-            order: 2
-        });
-	}
+function config(modulesMenuServiceProvider, $stateProvider, USER_ROLES) {
+    modulesMenuServiceProvider.addItem({
+        templateUrl: 'app/src/contacts/view/menuItem.html',
+        order: 2
+    });
+    
+    $stateProvider.state('administration.organizations', {
+            url: '/organizations',
+            data: {
+                authorizedRoles: [USER_ROLES.admin],
+                searchContext: 'CONTACT_ORGANISATIONS',
+                selectedTab: 2
+            },
+            views: {
+                'organizations': {
+                    templateUrl: 'app/src/contacts/view/organizations.html'
+                }
+            }
+        }).state('administration.organization', {
+            url: '/organization/:storeProtocol/:storeIdentifier/:uuid',
+            data: {
+                authorizedRoles: [USER_ROLES.admin],
+                selectedTab: 2
+            },
+            views: {
+                'organizations': {
+                    templateUrl: 'app/src/contacts/view/organization.html'
+                }
+            }
+        }).state('administration.contacts', {
+            url: '/contacts',
+            data: {
+                authorizedRoles: [USER_ROLES.admin],
+                searchContext: 'CONTACT_USERS',
+                selectedTab: 3
+            },
+            views: {
+                'contacts': {
+                    templateUrl: 'app/src/contacts/view/persons.html'
+                }
+            }
+        }).state('contacts', {
+        parent: 'site',
+        url: '/contacts',
+        data: {
+            authorizedRoles: [USER_ROLES.user],
+            selectedTab: 0
+        },
+        views: {
+            'content@': {
+                templateUrl: 'app/src/contacts/view/contacts.html',
+                controller: 'ContactsController',
+                controllerAs: 'vm'
+            }
+        }
+    }).state('contacts.persons', {
+        url: '/persons',
+        data: {
+            authorizedRoles: [USER_ROLES.user],
+            selectedTab: 0,
+            hideHeader: true
+        },
+        views: {
+            'persons': {
+                templateUrl: 'app/src/contacts/view/persons.html'
+            }
+        }
+    }).state('contacts.organizations', {
+        url: '/organizations',
+        data: {
+            authorizedRoles: [USER_ROLES.user],
+            selectedTab: 1,
+            hideHeader: true
+        },
+        views: {
+            'organizations': {
+                templateUrl: 'app/src/contacts/view/organizations.html'
+            }
+        }
+    }).state('contacts.organization', {
+        url: '/organization/:storeProtocol/:storeIdentifier/:uuid',
+        views: {
+            'organizations': {
+                templateUrl: 'app/src/contacts/view/organization.html'
+            }
+        },
+        data: {
+            authorizedRoles: [USER_ROLES.user],
+            selectedTab: 1
+        }
+    });
+}
